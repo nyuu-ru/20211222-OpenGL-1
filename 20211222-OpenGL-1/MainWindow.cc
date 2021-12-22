224986c3963a735f9cb4022318abee063a69e919
evilunyuu <u@nyuu.ru>
2021-12-22 13:04:19 +0500

    Повернули и переместили цветной треугольник

--- 20211222-OpenGL-1/MainWindow.cc
+++ 20211222-OpenGL-1/MainWindow.cc
@@ -21,9 +21,17 @@ void MainWindow::render()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	glLoadIdentity();
+	glTranslatef(0.5f, 0.0f, 0.0f);
+	glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
+
 	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.7f, 0.2f);
 	glVertex2f(  0.0f,  0.8f);
+	glColor3f(0.0f, 0.0f, 0.5f);
 	glVertex2f(  0.8f, -0.8f);
+	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex2f( -0.8f, -0.8f);
 	glEnd();
+
 }
